97e05c5136831ab1cab8613156eab4aad24b5886
Gabriel Teixeira <biel.gt@gmail.com>
2021-09-01 11:29:15 -0300

    Versionamento
    Criando metodo de versionamento

---
+++ GameDev001/Assets/Scripts/_VersionamentoProjeto.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class _VersionamentoProjeto : MonoBehaviour
+{
+    /* HELP Versionamento
+     * x. Master - Quando há uma mudança muito grande no projeto
+     * x. Minor - Sempre que adicionar uma funcionalidade novo (Como missão, char, cidade, vila, vilão, mapa...)
+     * x. build - Sempre que realizar qualquer mudança.
+     * xxxx data - Referente a data que editou, por exemplo, setembro de 2021 ficaria, (21 -> do ano + 09 -> referente ao mes) logo 2109         
+     */
+
+    private string versao = "v1.0.0.2109";
+
+    #region Codigo
+    public TextMeshProUGUI txtVersao;
+
+    private void Start()
+    {
+        txtVersao.text = versao;
+    }
+    #endregion
+}

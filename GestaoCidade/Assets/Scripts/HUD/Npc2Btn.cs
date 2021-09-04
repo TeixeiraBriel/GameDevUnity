8a075d59d814a86eef56df91c350b8a3d7161367
Gabriel Teixeira <biel.gt@gmail.com>
2021-09-04 13:55:58 -0300

    Reproduzir
    Ao juntar vinte de cada cor o botão reproduzir ira ativar e assim podendo gerar um segundo npc.

---
+++ GestaoCidade/Assets/Scripts/HUD/Npc2Btn.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Npc2Btn : MonoBehaviour
+{
+    public Button repro;
+    public GameObject npc2;
+
+    private void Start()
+    {
+        repro.interactable = false;
+    }
+
+    private void Update()
+    {
+        if (ContadorColeta.contadorCubosAzuis == 20 && ContadorColeta.contadorCubosRosas == 20)
+        {
+            repro.interactable = true;
+        }
+    }
+
+    public void dupli()
+    {
+        npc2.active = true;
+        ContadorColeta.contadorCubosAzuis -= 20;
+        ContadorColeta.contadorCubosRosas -= 20;
+        repro.interactable = false;
+    }
+}

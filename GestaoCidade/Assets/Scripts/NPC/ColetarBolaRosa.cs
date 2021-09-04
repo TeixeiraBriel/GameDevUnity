4dcd64300f0060cb7d8402c0e71ae9793935cf88
Gabriel Teixeira <biel.gt@gmail.com>
2021-09-04 14:01:10 -0300

    Limpando Codigo

--- GestaoCidade/Assets/Scripts/NPC/ColetarBolaRosa.cs
+++ GestaoCidade/Assets/Scripts/NPC/ColetarBolaRosa.cs
@@ -12,8 +12,6 @@ public class ColetarBolaRosa : MonoBehaviour
     public Transform fonteBolaRosa;
     public Transform cabana;
 
-    private bool cargaMaximaAtingida;
-
     // Start is called before the first frame update
     void Start()
     {
